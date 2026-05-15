70dc66e558f41d1c578dc1e09bbc67cca4c6198b
RamigoLab <ramigolab@gmail.com>
2026-05-15 23:08:58 +0200

    Delete src/config/constants.js

--- src/config/constants.js
+++
@@ -1,5 +0,0 @@
-// Imposta a false per disabilitare Supabase e usare i dati mock offline
-export const USE_SUPABASE = true;
-
-export const APP_VERSION = '1.0.0';
-export const APP_NAME = 'PolisRoad';
