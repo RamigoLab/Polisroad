2a5cf9cd8deb4e68562a373bc5f8527200783db7
RamigoLab <ramigolab@gmail.com>
2026-06-19 23:56:42 +0200

    1.6.1

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       devOptions: {
         enabled: process.env.VITE_PWA_DEV === 'true'
       },
-      includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
+      includeAssets: ['icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-192-maskable.png', 'icons/icon-512-maskable.png'],
       manifest: {
         name: 'PolisRoad',
         short_name: 'PolisRoad',
@@ -30,6 +30,18 @@ export default defineConfig({
             src: '/icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png'
+          },
+          {
+            src: '/icons/icon-192-maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/icons/icon-512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
